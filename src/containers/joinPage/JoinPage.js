e6ba6e19f221e7fce7569eed971350368746d6bd
taz717 <moatazkhallaf@outlook.com>
2022-11-24 18:57:32 -0700

    General Additions -add boxes for homepage -add routing for 3 pages -> Member -> HomePage -> Join

---
+++ src/containers/joinPage/JoinPage.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+function JoinPage() {
+    return (
+        <Box>JoinPage</Box>
+    );
+}
+
+export default JoinPage;
