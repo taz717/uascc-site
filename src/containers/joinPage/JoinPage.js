457e68c6f46b56192d619e0ddbdcda57dbc31ac3
Moataz Khallaf <MoatazKhallaf@outlook.com>
2023-06-05 13:37:09 -0600

    img and survey fix
    - set images to all be the same aspect ratio
    - set google form to be fully displayed without scrollbar
    - added lazy loading for images

--- src/containers/joinPage/JoinPage.js
+++ src/containers/joinPage/JoinPage.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
 function JoinPage() {
@@ -18,11 +17,18 @@ function JoinPage() {
             <Grid item xs={3}>
                 <Card sx={{ margin: '16px' }}>
                     <CardContent>
-                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfNSCZPpI0ieE8p3tsccHMQLFG5HphU9oc8WZx4B3bzjD0FTg/viewform?embedded=true" scrolling='no' width="640" height="2244" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                        <iframe
+                            title="Join Form"
+                            src="https://docs.google.com/forms/d/e/1FAIpQLSfNSCZPpI0ieE8p3tsccHMQLFG5HphU9oc8WZx4B3bzjD0FTg/viewform?embedded=true"
+                            width="640"
+                            height="2250"
+                            frameborder="0"
+                            marginheight="0"
+                            marginwidth="0">Loading…</iframe>
                     </CardContent>
                 </Card>
             </Grid>
-        </Grid>
+        </Grid >
     );
 }
 
